03dfe938087363b961cda7c6d35556ef3cad3970
Rocky0524 <kenbalboa05@gmail.com>
2024-12-12 20:03:22 +0100

    part2
    En esta parte arreglo un problema con los turnos ademas de que el juego empieza a ser funcional ya

--- Tresenraya/src/paquete3enraya/tresenrayabien.java
+++ Tresenraya/src/paquete3enraya/tresenrayabien.java
@@ -56,13 +56,59 @@ System.out.println("");
 }
 
 
+		int moneda = random.nextInt(2);
+		System.out.println("Empieza " + nomjugadores[moneda]);
 
 
+		String simbolo1 = "X";  // Jugador 1
+		String simbolo2 = "O";  // Jugador 2
+		
+		int turno = 0;
+		
+		while (true) {
 
+			int fila;
+			int columna;
+			boolean jugadaValida;
+			
+				System.out.println("\nTurno de " + nomjugadores[turno]);
 
 break;
 }
 		
+				for (int i = 0; i < matriz.length; i++) { // primero recore las filas
+					for (int j = 0; j < matriz[i].length; j++) { // segundo recorre las columnas
+						System.out.print("|" + matriz[i][j] + "|");
+					}
+					System.out.println("");
+				}
+				
+				 // El jugador elige una posición
+			    jugadaValida = true;
+			    while (jugadaValida) {
+			        System.out.println("Elige una fila (0, 1, 2): ");
+			        fila = s.nextInt();
+			        System.out.println("Elige una columna (0, 1, 2): ");
+			        columna = s.nextInt();
+			        
+			     // Verifica si la celda está vacía
+			        if (matriz[fila][columna].equals(".")) {
+			            if (turno == 0) {
+			                matriz[fila][columna] = simbolo1; // Jugador 1 usa "X"
+			            } else {
+			                matriz[fila][columna] = simbolo2; // Jugador 2 usa "O"
+			            }
+			            jugadaValida = false; // La jugada es válida, se sale del bucle
+			        } else {
+			            System.out.println("Esa posición ya está ocupada. Intenta de nuevo.");
+			        }
+			}
+			    
+			 // Alternar turno entre los jugadores
+	            turno = (turno + 1) % 2; // Cambiar entre 0 y 1 (alternando turnos)
+	            
+			
+	}
 	}
 
 }
